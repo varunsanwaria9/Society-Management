4d802332abaa2ec9d6fe7228ab49f1a6d09f0ee3
Varun <vrn@pop-os.localdomain>
2021-10-10 00:46:55 +0530

    initial commit

---
+++ society-backend/src/main/java/com/example/societybackend/SocietyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.societybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocietyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocietyBackendApplication.class, args);
+	}
+
+}
