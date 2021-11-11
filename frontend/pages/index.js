e0497e1f6fdc8900b310f65f6130f86bbd1b2293
Varun Sanwaria <Varun Sanwaria>
2021-11-12 00:39:33 +0530

    Added electron support

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,14 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 // Path: localhost:3000/
 
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-        <p>Landing Page</p>
-    </div>
-  )
+	return (
+		<div className={styles.container}>
+			<p>Home Page</p>
+			<br />
+			<Link href="/login">
+				<p>Login Page</p>
+			</Link>
+		</div>
+	)
 }
