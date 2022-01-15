5aefe2b7b407d01afdbe2e3d6f23efeaa126c833
Varun Sanwaria <varunsanwaria9@gmail.com>
2022-01-15 16:46:18 +0530

    Removed Supervisor in backend

--- Spring-Backend/src/main/java/com/example/SpringBackend/database/repos/SupervisorRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.SpringBackend.database.repos;
-
-import com.example.SpringBackend.database.entities.Supervisor;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SupervisorRepo extends JpaRepository<Supervisor, Integer> {
-    
-}
