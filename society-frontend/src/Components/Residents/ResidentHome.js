b658f90d3fd216750ef30b06d1eb4abc46bd51de
Varun Sanwaria <varunsanwaria9@gmail.com>
2022-05-05 01:12:16 +0530

    Updated Resident Home page

--- society-frontend/src/Components/Residents/ResidentHome.js
+++ society-frontend/src/Components/Residents/ResidentHome.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ResidentNavbar from './ResidentNavbar';
+import { Canvas } from '@react-three/fiber';
+import { Html } from '@react-three/drei';
+import Person from '../3dModels/Person1'
+
+import '../styles/Residents/HomePage.css'
 
 export default function ResidentHome() {
   return (
-    <div>
+    <div className='resident-home-main'>
         <ResidentNavbar />
-        <h1>ResidentHome</h1>
+        <div className='resident-home-canvas'>
+          <Canvas className='resident-home-canvas'>
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[-2, 5, 2]} />
+            <Person />
+            <Html>
+              <div className='resident-homed-div'>
+                <p className='resident-home-msg'>Welcome</p>
+                <p className='resident-home-msg'>Resident</p>
+              </div>
+            </Html>
+          </Canvas>
+        </div>
     </div>
   );
 }
